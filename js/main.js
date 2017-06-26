e9f8585e52f789372aede325027c153d25dfa343
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-06-26 10:04:31 -0700

    Clean code

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function initialize(){
     squares.forEach(function(el){
         el.textContent = '';
     });
-    whosWinner = squareClicked =  '';
+    whosWinner = squareClicked = winner.textContent = '';
     // Event Listeners
     tbodyEl.addEventListener('click', handleClick);
     buttonEl.addEventListener('click', initialize);
@@ -37,6 +37,7 @@ function handleClick(evt){
         render();
         turn = !turn;
     }    
+    if(whosWinner) tbodyEl.removeEventListener('click', handleClick);
 }
 
 function render(){
@@ -59,35 +60,28 @@ function checkWin(){
     // check top row
     if(board[0] !== null && board[0] === board[1] && board[0] === board[2]){ 
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // check middle row
     else if(board[3] !== null && board[3] === board[4] && board[3] === board[5]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
+        // tbodyEl.removeEventListener('click', handleClick);
     } // check bottom row 
     else if(board[6] !== null && board[6] === board[7] && board[6] === board[8]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // check left column 
     else if(board[0] !== null && board[0] === board[3] && board[0] === board[6]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // check middle column 
     else if(board[1] !== null && board[1] === board[4] && board[1] === board[7]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // check right colum 
     else if(board[2] !== null && board[2] === board[5] && board[2] === board[8]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // check left to right \ 
     else if(board[0] !== null && board[0] === board[4] && board[0] === board[8]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // check right to left / 
     else if(board[2] !== null && board[2] === board[4] && board[2] === board[6]){
         whosWinner = turn ? player1 : player2;
-        tbodyEl.removeEventListener('click', handleClick);
     } // it's a tie 
     else {
         // check to see if the board is empty.
